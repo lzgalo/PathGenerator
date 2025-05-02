f38c10dc32217f02f993c51aab6636dd2ad96e1a
Luiz Galo <lz.eduardogalo@gmail.com>
2025-05-02 16:38:51 -0300

    add: src files

---
+++ src/main.cpp
@@ -0,0 +1,115 @@
+#include <queue>
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <matplotlibcpp.h>
+
+namespace plt = matplotlibcpp;
+
+int dx[] = {-1, 1, 0, 0};
+int dy[] = {0, 0, -1, 1};
+
+bool isValid(
+    int x, int y, int n, int m, std::vector<std::vector<int>>& maze,
+    std::vector<std::vector<bool>>& visited
+    ) {
+    return (x >= 0 && x < n && y >= 0 && y < m && maze[x][y] == 0 && !visited[x][y]);
+}
+
+std::vector<std::pair<int, int>> bfs(
+    std::vector<std::vector<int>>& maze,
+    std::pair<int, int> start,
+    std::pair<int, int> end, int n, int m
+    ) {
+
+    std::vector<std::vector<bool>> visited(n, std::vector<bool>(m, false));
+    std::vector<std::vector<std::pair<int, int>>> parent(n, std::vector<std::pair<int, int>>(m));
+    std::queue<std::pair<int, int>> q;
+
+    q.push(start);
+    visited[start.first][start.second] = true;
+
+    while (!q.empty()) {
+        std::pair<int, int> current = q.front();
+        q.pop();
+
+        if (current == end) {
+            std::vector<std::pair<int, int>> path;
+            std::pair<int, int> temp = end;
+            while (temp != start) {
+                path.push_back(temp);
+                temp = parent[temp.first][temp.second];
+            }
+            path.push_back(start);
+            std::reverse(path.begin(), path.end());
+            return path;
+        }
+
+        for (int i = 0; i < 4; i++) {
+            int nx = current.first + dx[i];
+            int ny = current.second + dy[i];
+
+            if (isValid(nx, ny, n, m, maze, visited)) {
+                visited[nx][ny] = true;
+                parent[nx][ny] = current;
+                q.push({nx, ny});
+            }
+        }
+    }
+
+    return {};
+}
+
+void plotMazeWithPath(
+    std::vector<std::vector<int>>& maze,
+    std::vector<std::pair<int, int>>& path,
+    int n, int m
+    ) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (maze[i][j] == 1) {
+                std::vector<int> x = {j, j + 1, j + 1, j, j};
+                std::vector<int> y = {n - i, n - i, n - i - 1, n - i - 1, n - i};
+                plt::plot(x, y, {{"color", "black"}, {"linewidth", "1"}});
+            }
+        }
+    }
+
+    std::vector<double> x, y;
+    for (auto& p : path) {
+        x.push_back(p.second + 0.5);
+        y.push_back(n - p.first - 0.5);
+    }
+
+    plt::plot(x, y, {{"color", "red"}, {"linewidth", "2"}});
+    plt::xlim(0, m);
+    plt::ylim(0, n);
+    plt::axis("off");
+    plt::show();
+}
+
+int main() {
+    int n = 10, m = 10;
+    std::vector<std::vector<int>> maze = {
+        {0, 1, 1, 0, 0, 0, 1, 1, 1, 0},
+        {0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
+        {1, 0, 0, 0, 1, 0, 0, 0, 1, 1},
+        {1, 0, 0, 1, 0, 0, 1, 0, 0, 0},
+        {0, 0, 0, 0, 1, 0, 1, 0, 0, 0},
+        {0, 1, 0, 1, 0, 0, 1, 0, 1, 1},
+        {1, 0, 0, 0, 1, 0, 1, 0, 0, 0},
+        {1, 1, 0, 1, 1, 0, 0, 1, 0, 1},
+        {0, 1, 1, 0, 0, 1, 0, 0, 0, 0},
+        {0, 0, 1, 1, 0, 1, 0, 1, 0, 0}
+    };
+    std::pair<int, int> start = {0, 0};
+    std::pair<int, int> end = {9, 9};
+
+    std::vector<std::pair<int, int>> path = bfs(maze, start, end, n, m);
+
+    if (!path.empty()) {
+        plotMazeWithPath(maze, path, n, m);
+    }
+
+    return 0;
+}
